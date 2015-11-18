6f01953a75a74969e9f9862b6c24870a9dd07680
Ryan Foster <rfosterslo@gmail.com>
2015-11-18 08:33:57 -0800

    Added unicode literals

--- wagtailplus/wagtaillinks/models.py
+++ wagtailplus/wagtaillinks/models.py
@@ -1,6 +1,8 @@
 """
 Contains model class definitions.
 """
+from __future__ import unicode_literals
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy  as _
