fb202fb18f3d7e61e0448901dbe5d69ad560bccd
Ryan Foster <rfosterslo@gmail.com>
2015-11-25 15:24:24 -0800

    Added get_related_entry_admin_url

--- wagtailplus/wagtailrelations/templatetags/wagtailrelations_tags.py
+++ wagtailplus/wagtailrelations/templatetags/wagtailrelations_tags.py
@@ -2,13 +2,14 @@
 Contains application template tags.
 """
 from django import template
+from django.core import urlresolvers
 
 from ..models import Entry
 
 
 register = template.Library()
 
-@register.assignment_tag()
+@register.assignment_tag
 def get_related(page):
     """
     Returns list of related Entry instances for specified page.
@@ -24,6 +25,20 @@ def get_related(page):
 
     return related
 
+@register.simple_tag
+def get_related_entry_admin_url(entry):
+    """
+    Returns admin URL for specified entry instance.
+
+    :param entry: the entry instance.
+    :return: str.
+    """
+    return urlresolvers.reverse(
+        'admin:%s_%s_change'.format(entry.content_type.app_label, entry.content_type.model),
+        args=(entry.object_id,)
+    )
+
+@register.assignment_tag
 def get_related_with_scores(page):
     """
     Returns list of related tuples (Entry instance, score) for
